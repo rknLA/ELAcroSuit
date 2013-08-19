ee0405ad1cdc17131db6d5a2c8c4685168ff0b12
leslie bienenfeld <lbienenfeld@gmail.com>
2013-08-18 23:06:16 -0700

    A bit of a binary counter light show.

--- el_seq/el_seq.ino
+++ el_seq/el_seq.ino
@@ -4,7 +4,7 @@
  * by Leslie Bienenfeld and Kevin Nelson 
  */
  
-//#define DEBUG
+#define DEBUG
 
 // oh arduino... there must be a better way.
 // EL Driver labels and their corresponding digital pins
@@ -25,13 +25,28 @@
 #define LEFT_FOOT A6
 #define RIGHT_FOOT A7
 
+
 int el_pins[] = {EL_A, EL_B, EL_C, EL_D, EL_F, EL_G, EL_H};
 int el_count = 8;
 
+int sensor_pins[] = {LEFT_SHOULDER,
+                     RIGHT_SHOULDER,
+                     LEFT_HIP,
+                     RIGHT_HIP,
+                     LEFT_FOOT,
+                     RIGHT_FOOT};
+int sensor_count = 6;
+
 void setup() {
-  // set EL Wire pins to output
+  // intialize EL wire pins
   for (int i = 0; i < el_count; ++i) {
     pinMode(el_pins[i], OUTPUT);
+    
+    // initialize sensor pins too
+    if (i < sensor_count) {
+      pinMode(sensor_pins[i], INPUT);
+      digitalWrite(sensor_pins[i], HIGH); // use the internal pull-up resistor
+    }
   }
 
 #ifdef DEBUG
@@ -44,16 +59,27 @@ void setup() {
 }
 
 // for now, just display some el blinkies.
+byte lights = 0;
+byte stage = 0;
+unsigned long change_time = 0;
 void loop() {
   unsigned long now = millis();
-  unsigned long seconds = now / 1000;
-  if (now % 1000 == 0) { 
-    for (int i = 0; i < el_count; ++i) {
-      if (seconds % i == 0) {
-         digitalWrite(el_pins[i], HIGH);
-      } else {
-        digitalWrite(el_pins[i], LOW);
-      }
-    }
+  if (now - change_time > 500) {
+    stage = (stage + 1); //% el_count;
+
+    write_lights(stage);
+    change_time = now;
+  }
+  
+  Serial.print("Sensor value: ");
+  Serial.print(analogRead(A7));
+  Serial.print("\n");
+}
+
+void write_lights(byte light_mask) {
+  // loop through the byte to write pins.. super inefficient. whatever
+  for (int i = 0; i < el_count; ++i) {
+    boolean state = light_mask & (1 << i);
+    digitalWrite(el_pins[i], state);
   }
 }
